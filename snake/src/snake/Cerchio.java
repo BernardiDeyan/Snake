45bf842545b9c5914e727c452ad012318a88cd49
BernardiDeyan <deyan.bernardi@gmail.com>
2016-02-20 12:07:49 +0100

    20

--- snake/src/snake/Cerchio.java
+++ snake/src/snake/Cerchio.java
@@ -2,57 +2,71 @@ package snake;
 
 public class Cerchio {
 	
-	private Punto centro;
+	private int x;
+	private int y;
 	private int raggio;
 	
-	public Cerchio(Punto centro, int raggio) { 
-		this.centro=new Punto(centro);
+	public Cerchio(int x1,int y2, int raggio) { 
+		this.x=x1;
+		this.y=y2;
         setRaggio(raggio);
 	}
-	
-	
-	public Punto getCentro() {
-		Punto centro=new Punto(this.centro);
-		return centro;
+
+//////////////GETTER SETTER//////////////////////
+	public int getX() {
+		return x;
 	}
-	public void setCentro(Punto centro) {
-		this.centro = centro;
+
+	public void setX(int x) {
+		this.x = x;
 	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
 	public int getRaggio() {
 		return raggio;
 	}
+
 	public void setRaggio(int raggio) {
 		this.raggio = raggio;
 	}
-	
-	
+//////////////////////////////////////////////
+
 	public void spostaSu(int deltax, int deltay){
 		
-		centro.setX(centro.getX()+deltax);
-		
-		centro.setY(centro.getY()+deltay);
+		this.x+=deltax;
+		this.y+=deltay;
 	
 	}
 	public void spostaGiu(int deltax, int deltay){
 		
-		centro.setX(centro.getX()+deltax);
-		
-		centro.setY(centro.getY()+deltay);
+		this.x-=deltax;
+		this.y-=deltay;
 	
 	}
 	public void spostaDx(int deltax, int deltay){
 	
-		centro.setX(centro.getX()+deltax);
-	
-		centro.setY(centro.getY()+deltay);
+		this.x+=deltax;
+		this.y+=deltay;
 	
 	}
 	public void spostaSx(int deltax, int deltay){
 	
-		centro.setX(centro.getX()+deltax);
-	
-		centro.setY(centro.getY()+deltay);
-	
+		this.x-=deltax;
+		this.y-=deltay;
 	}
+
+
+	public String toString() {
+		return "Cerchio [x=" + x + ", y=" + y + ", raggio=" + raggio + "]";
+	}
+
+	
 
 }
--- snake/src/snake/snake.java
+++ snake/src/snake/snake.java
@@ -13,14 +13,18 @@ import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.events.ShellAdapter;
+import org.eclipse.swt.events.ShellEvent;
 
 public class snake {
 
 	protected Shell shell;
 	GC gc;
 	int raggio=10;
-	Punto centro=new Punto(150,100);
-	Cerchio cerchio=new Cerchio(centro,raggio);
+	int x=150;
+	int y=100;
+	//Punto centro=new Punto(150,100);
+	Cerchio cerchio=new Cerchio(x,y,raggio);
 	
 	/**
 	 * Launch the application.
@@ -49,24 +53,36 @@ public class snake {
 			}
 		}
 	}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-public void spostaSu(int deltax, int deltay){
+	
+/////////////FUNZIONE GENERA MELE////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	public void generaMela(){
 		
-		centro.setX(centro.getX()+deltax);
+		//int x=
+		//int y=
+		//gc.drawOval(,, raggio/2,raggio/2);
 		
-		centro.setY(centro.getY()+deltay);
+		
+		
 	
 	}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
-	
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
 
 	/**
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
 		shell = new Shell();
+		shell.addShellListener(new ShellAdapter() {
+			@Override
+			public void shellActivated(ShellEvent e) {
+				
+				Punto.xmin = 5;
+				Punto.ymin = 5;
+				Punto.xmax= 595;
+				Punto.ymax=295;
+				
+			}
+		});
 		shell.setBackground(SWTResourceManager.getColor(SWT.COLOR_TITLE_BACKGROUND));
 		shell.setSize(641, 528);
 		shell.setText("SWT Application");
@@ -77,7 +93,7 @@ public void spostaSu(int deltax, int deltay){
 			public void paintControl(PaintEvent arg0) {
 				gc=new GC(canvas);
 				
-				gc.drawOval(centro.getX(),centro.getY(), raggio,raggio);
+				gc.drawOval(cerchio.getX(),cerchio.getY(), raggio,raggio);
 				
 				gc.dispose();
 				
@@ -87,7 +103,7 @@ public void spostaSu(int deltax, int deltay){
 		});
 				
 		canvas.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-		canvas.setBounds(10, 148, 605, 332);
+		canvas.setBounds(10, 148, 600, 300);
 		
 
 		
@@ -97,8 +113,10 @@ public void spostaSu(int deltax, int deltay){
 			public void widgetSelected(SelectionEvent e) {
 				gc=new GC(canvas);
 				
+				System.out.println(cerchio);
 				cerchio.spostaSu(0,-10);
-				gc.drawOval(centro.getX(),centro.getY(), raggio,raggio);
+				System.out.println(cerchio);
+				gc.drawOval(cerchio.getX(),cerchio.getY(), raggio,raggio);
 				
 				gc.dispose();
 				
@@ -108,14 +126,56 @@ public void spostaSu(int deltax, int deltay){
 		btnSu.setText("^");
 		
 		Button btnGiu = new Button(shell, SWT.NONE);
+		btnGiu.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {	
+				gc=new GC(canvas);
+				
+				System.out.println(cerchio);
+				cerchio.spostaGiu(0,-10);
+				System.out.println(cerchio);
+				
+				gc.drawOval(cerchio.getX(),cerchio.getY(), raggio,raggio);
+				
+				
+				gc.dispose();
+			}
+		});
 		btnGiu.setText("v");
 		btnGiu.setBounds(284, 117, 32, 25);
 		
 		Button btnDx = new Button(shell, SWT.NONE);
+		btnDx.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				
+				gc=new GC(canvas);
+				
+				System.out.println(cerchio);
+				cerchio.spostaDx(+10,0);
+				System.out.println(cerchio);
+				gc.drawOval(cerchio.getX(),cerchio.getY(), raggio,raggio);
+				
+				gc.dispose();
+			}
+		});
 		btnDx.setText(">");
 		btnDx.setBounds(322, 117, 32, 25);
 		
 		Button btnSx = new Button(shell, SWT.NONE);
+		btnSx.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				gc=new GC(canvas);
+				
+				System.out.println(cerchio);
+				cerchio.spostaSu(-10,0);
+				System.out.println(cerchio);
+				gc.drawOval(cerchio.getX(),cerchio.getY(), raggio,raggio);
+				
+				gc.dispose();
+			}
+		});
 		btnSx.setText("<");
 		btnSx.setBounds(246, 117, 32, 25);
 		
@@ -124,6 +184,17 @@ public void spostaSu(int deltax, int deltay){
 		lblSnake.setFont(SWTResourceManager.getFont("Segoe UI Light", 30, SWT.BOLD));
 		lblSnake.setBounds(246, 10, 120, 54);
 		lblSnake.setText("Snake");
+		
+		Button btnStart = new Button(shell, SWT.NONE);
+		btnStart.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				
+				
+			}
+		});
+		btnStart.setBounds(375, 117, 75, 25);
+		btnStart.setText("Start");
 
 	}
 }
